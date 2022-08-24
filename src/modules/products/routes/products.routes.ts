17496fff7be1ac0ff4a001829aae97a5d3e192a4
guilherme <guilherme@digitalone.com.br>
2022-08-24 14:02:46 -0300

    product data validations

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,13 +1,53 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/ProductsController';
+import { Joi, celebrate, Segments } from 'celebrate';
 
 const productsRouter = Router();
 const productsController = new ProductsController();
 
 productsRouter.get('/', productsController.get);
-productsRouter.get('/:id', productsController.getByid);
-productsRouter.post('/', productsController.create);
-productsRouter.put('/:id', productsController.update);
-productsRouter.delete('/:id', productsController.delete);
+productsRouter.get(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+    }),
+    productsController.getByid,
+);
+productsRouter.post(
+    '/',
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            price: Joi.number().required(),
+            quantity: Joi.number().required(),
+        },
+    }),
+    productsController.create,
+);
+productsRouter.put(
+    '/:id',
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            price: Joi.number().required(),
+            quantity: Joi.number().required(),
+        },
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+    }),
+    productsController.update,
+);
+productsRouter.delete(
+    '/:id',
+    celebrate({
+        [Segments.PARAMS]: {
+            id: Joi.string().uuid().required(),
+        },
+    }),
+    productsController.delete,
+);
 
 export default productsRouter;
